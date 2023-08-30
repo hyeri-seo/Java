3118cae67b8c82e05587f603ce86ad057bd728b8
hyeri-seo <hye_dam_2@naver.com>
2023-08-30 18:18:58 +0900

    java

--- BankProj/src/Bank.java
+++ BankProj/src/Bank.java
@@ -1,4 +1,7 @@
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 import acc.Account;
@@ -7,9 +10,7 @@ import exc.BankError;
 import exc.BankException;
 
 public class Bank {
-	private static final int case1 = 0;
-	Account[] accs = new Account[100];
-	int accCnt;
+	List<Account> accs = new ArrayList<>();
 	Scanner sc = new Scanner(System.in);
 
 	int menu() throws BankException {
@@ -63,7 +64,7 @@ public class Bank {
 		String name = sc.nextLine();
 		System.out.print("입금액:");
 		int money = Integer.parseInt(sc.nextLine());
-		accs[accCnt++]=new Account(id,name,money);
+		accs.add(new Account(id,name,money));
 	}
 	
 	void makeSpecialAccount() {
@@ -82,14 +83,17 @@ public class Bank {
 		System.out.print("등급(VIP-V, Gold-G, Silver-S, Normal-N):");
 		String grade = sc.nextLine();
 		//추가
-		accs[accCnt++] = new SpecialAccount(id,name,money,grade);
+		accs.add(new SpecialAccount(id,name,money,grade));
 	}
 	
 	Account searchAccById(String id) {
-		for(int i=0; i<accCnt; i++) {
-			if(accs[i].getId().equals(id)) {	//id 값을 비교해줘야 하니까 set이 아니라 get을 써야 함.
-				return accs[i];
-			}
+//		for(int i=0; i<accs.size(); i++) {
+//			if(accs.get(i).getId().equals(id)) {	//id 값을 비교해줘야 하니까 set이 아니라 get을 써야 함.
+//				return accs.get(i);
+//			}
+//		}
+		for(Account acc: accs) {
+			if(acc.getId().equals(id)) return acc;
 		}
 		return null;
 	}
@@ -131,9 +135,15 @@ public class Bank {
 	}
 	
 	void allAccountInfo() {
-		for(int i=0; i<accCnt; i++) {
-			System.out.println(accs[i]);
+		System.out.println("[전체 계좌 조회]");
+		Iterator<Account> it = accs.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
 		}
+		
+//		for(Account acc: acs) {
+//			System.out.println(acc);
+//		}
 	}
 	
 	public static void main(String[] args) {
